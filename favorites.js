d875c7bffdef715f7360c2a6a24d7954c9027211
joantriana7-afk <joantriana7@gmail.com>
2026-04-13 11:59:42 -0500

    otroa cambios en favorites

--- favorites.js
+++ favorites.js
@@ -5,10 +5,10 @@ function saveFavorite() {
     }
 
     let texto = localStorage.getItem("favoritos");    // getIteam es leer algo, en este caso esta leyendo los favoritos
-    let lista = JSON.parse(texto) || [];             //JSON.parse es convertir texto a un array/objeto
+    let lista = JSON.parse(texto) || [];             //JSON.parse es convertir texto a un array/objeto 
 
     let yaExiste = lista.find(function(p) {      // find es que recorre  la lista buscando el mismo nombre
-    return p.nombre === pokemonActual.nombre;   // (p) es cada elemento de la lista y find es que recorre la lista buscando el mismno nombre
+    return p.nombre === pokemonActual.nombre;   // (p) es cada elemento de la lista 
     });
     if (yaExiste) {
         alert("¡" + pokemonActual.nombre + "ya existe")
